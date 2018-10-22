f05833cb3c203d5cb6017ee00c9c690452b2c71c
axjiang14 <axjiang14@gmail.com>
2018-10-22 11:20:24 -0500

    add home menu

---
+++ gameStartState.js
@@ -0,0 +1,30 @@
+var gameStartState = {
+	preload: function()
+	{
+		game.load.image('homeScreen', 'assets/homescreen.png');
+        game.load.image('startButton', 'assets/start.png');
+        
+	},
+	
+	create: function()
+	{
+        game.add.sprite(0, 0, 'homeScreen');
+		
+		
+        var startButton = game.add.button(game.world.centerX + 50, game.world.centerY + 125, 'startButton', function(){game.state.start('solarSystem')});// go to solarSystem.
+        startButton.onInputDown.add(this.tint, startButton);
+        startButton.onInputUp.add(this.unTint, startButton);        
+        
+	},
+	
+	update: function(){},
+    
+    tint: function(){
+        this.tint = 0xbbbbbb;
+        //buttonBGM.play('click');
+    },
+    
+    unTint: function(){
+        this.tint = 0xFFFFFF;
+    },
+}; //game start state
