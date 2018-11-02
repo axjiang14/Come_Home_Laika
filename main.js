c84c1c956f91d696e1a16606c9659eaaccb5ca0e
Conner <connernewman@gmail.com>
2018-11-02 03:48:45 -0500

    Aliens can be killed properly now

--- main.js
+++ main.js
@@ -2,21 +2,43 @@ var Alien1 = class {
 
 	constructor(x, y) {
 		this.x = x * 24;
-		this.y = y * 24 - 12;
+		this.y = y * 24 - 16;
 		this.spritesheet = 'alien1';
 		
 		this.phaserObj = _aliens.create(this.x, this.y, this.spritesheet);
+		this.phaserObj.Alien = this;
 		this.sightLine = new Phaser.Line(0, 0, 0, 0);
+		
+		this.phaserObj.animations.add('left', [0, 1, 2, 3], 10, true);
+		this.phaserObj.animations.add('right', [5, 6, 7, 8], 10, true);
 		
 		this.hasSight = false; /// Can we see the player?
 		this.grounded = 0; // # of ticks to stay still for
 		this.shootTicks = 0; // # of ticks before we shoot
+		this.moveTicks = 0; // # of ticks to move for
 	}
 	
 	handle() {
 		// Decrement counters
 		--this.grounded;
 		--this.shootTicks;
+		--this.moveTicks;
+		
+		// Set wrapper x/y to phaser object x/y
+		this.x = this.phaserObj.x;
+		this.y = this.phaserObj.y;
+		
+		// Stop moving
+		if(this.moveTicks <= 0) {
+			this.phaserObj.body.velocity.x = 0;
+		}
+		// Don't go past min/max x
+		if(this.x <= this.min_x) {
+			this.phaserObj.body.velocity.x = Math.max(0, this.phaserObj.body.velocity.x);
+		}
+		else if(this.x >= this.max_x) {
+			this.phaserObj.body.velocity.x = Math.min(0, this.phaserObj.body.velocity.x);
+		}
 		
 		// Get distance b/w this alien and the player
 		var px = player.body.x;
@@ -43,23 +65,48 @@ var Alien1 = class {
 		if(distance < 250 && this.hasSight) {
 			if(this.shootTicks <= 0) {
 				console.log('I would like to shoot the player.');
-				// Alien reload timer
-				this.shootTicks = 300;
 				this.shootAtPlayer();
-				
-				// Alien won't move
-				this.grounded = 150;
 			}
 		}
+		// If we're not attacking, let's consider moving
 		else {
 			if(this.grounded <= 0) {
 				console.log('I would like to move.');
-				this.grounded = 200 + Math.random() * 100;
+				this.move();
 			}
 		}
 	}
 	
 	shootAtPlayer() {
+		var bullet = alienBullets.create(this.x + 16, this.y + 16, 'bullet');
+		bullet.liveTicks = 30;
+		game.physics.arcade.moveToXY(bullet, player.x + 20, player.y + 20, bulletSpeed / 2);
+		bullet.rotation = game.physics.arcade.angleBetween(bullet, player);
+		
+		// Alien reload timer
+		this.shootTicks = 275 + Math.random() * 50;
+		// Alien won't move
+		this.grounded = 150;
+	}
+	
+	move() {
+		// Wait for 200-300 ticks before we move again
+		this.grounded = 200 + Math.random() * 100;
+		// Move for 100 ticks
+		this.moveTicks = 100;
+		// Move at a speed of 75-125
+		this.phaserObj.body.velocity.x = 75 + 50 * Math.random();
+
+		// Flip a coin to go right/left
+		var direction = Math.random() > 0.5 ? -1 : 1
+
+		this.phaserObj.body.velocity.x *= direction;
+		if(direction == 1) {
+			this.phaserObj.animations.play('right');
+		}
+		else {
+			this.phaserObj.animations.play('left');
+		}
 	}
 }
 
@@ -68,11 +115,11 @@ function handlePlayer() {
 }
 
 function fire() {
-    if(this.game != null && game.time.now > nextFire) {
-        nextFire = game.time.now + fireRate;
-    	var bullet = bullets.create(player.x + 12, player.y + 18, 'bullet');
-    	game.physics.arcade.moveToPointer(bullet, bulletSpeed);
-    }
+	if(this.game != null && game.time.now > nextFire) {
+		nextFire = game.time.now + fireRate;
+		var bullet = bullets.create(player.x + 12, player.y + 18, 'bullet');
+		game.physics.arcade.moveToPointer(bullet, bulletSpeed);
+	}
 }
 
 function handle_alien(player, alien, alienBullets) {
@@ -102,18 +149,18 @@ function handle_alien(player, alien, alienBullets) {
 			
 			// Flip a coin to go right/left
 			direction = Math.random() > 0.5 ? -1 : 1
-            alien.animations.add('left', [0, 1, 2, 3], 10, true);
-            alien.animations.add('right', [5, 6, 7, 8], 10, true);
-            
-            //console.log(direction);
-            
+			alien.animations.add('left', [0, 1, 2, 3], 10, true);
+			alien.animations.add('right', [5, 6, 7, 8], 10, true);
+			
+			//console.log(direction);
+			
 			alien.body.velocity.x *= direction;
-            if (direction == 1){
-                alien.animations.play('right')
-            }
-            else {
-                alien.animations.play('left')
-            }
+			if (direction == 1){
+				alien.animations.play('right')
+			}
+			else {
+				alien.animations.play('left')
+			}
 		}
 	}
 	if(--alien.move <= 0)
@@ -170,7 +217,7 @@ function onPlayerHit() {
 	if(player.hp >= 10){
 		player.hpBox.tint = 0x20ff00;
 		player.hp = 10;
-    }
+	}
 }
 
 function stateLoad(filename) {
@@ -236,18 +283,18 @@ function stateLoad(filename) {
 function everyPreload() {
 	game.load.image('background', 'assets/BGPluto.png');
 	game.load.image('neptuneBackground', 'assets/NeptuneBackground_test.png');
-    game.load.image('uranusBackground', 'assets/BGUranus.png');
-    game.load.image('saturnBackground', 'assets/BGSaturn.png');
+	game.load.image('uranusBackground', 'assets/BGUranus.png');
+	game.load.image('saturnBackground', 'assets/BGSaturn.png');
 	game.load.image('ground', 'assets/platform.png');
 	game.load.image('platform_tile', 'assets/DPlatformS.png');
 	game.load.image('tile_light', 'assets/LPlatformS.PNG');
 	game.load.spritesheet('alien1', 'assets/Alien1.png', 32, 40);
-    game.load.spritesheet('alien2', 'assets/LongAlien.png', 32, 48);
+	game.load.spritesheet('alien2', 'assets/LongAlien.png', 32, 48);
 	game.load.image('diamond', 'assets/diamond.png');
 	game.load.spritesheet('laika', 'assets/laika.png', 32, 48);
-    game.load.image('bullet', 'assets/Beam-Pink.png');
-    game.load.image('healthKit', 'assets/firstaid.png');  
-    game.load.audio('bgm', 'assets/spaceBGM.mp3');
+	game.load.image('bullet', 'assets/Beam-Pink.png');
+	game.load.image('healthKit', 'assets/firstaid.png');
+	game.load.audio('bgm', 'assets/spaceBGM.mp3');
 	game.load.image('white_tile', 'assets/white_rect.png');
 	game.load.image('spaceship', 'assets/Spaceship.PNG');
 	game.load.image('crosshair', 'assets/crosshair.png');
@@ -260,67 +307,67 @@ function everyCreate() {
 	platforms = game.add.group();
 	platforms.enableBody = true;
 
-    bullets = game.add.group();
-    bullets.enableBody = true;
-    bullets.physicsBodyType = Phaser.Physics.ARCADE;
-    bullets.createMultiple(300, 'bullet'); // NUMBER OF BULLETS ALLOWED;
-    bullets.setAll('checkWorldBounds', true);
-    bullets.setAll('outOfBoundsKill', true);
+	bullets = game.add.group();
+	bullets.enableBody = true;
+	bullets.physicsBodyType = Phaser.Physics.ARCADE;
+	bullets.createMultiple(300, 'bullet'); // NUMBER OF BULLETS ALLOWED;
+	bullets.setAll('checkWorldBounds', true);
+	bullets.setAll('outOfBoundsKill', true);
 	
 	game.physics.arcade.enable(player);
-    //player.body.collideWorldBounds= true;
+	//player.body.collideWorldBounds= true;
 	game.camera.follow(player);
 	player.body.bounce.y = 0;
 	
 	// User Interface
-    player.hp = 10;
+	player.hp = 10;
 	player.hpBox = game.add.sprite(200, 18, 'white_tile');
 	player.hpBox.scale.setTo(10, 1);
 	player.hpBox.tint = 0x20ff00;
-    player.hpBox.fixedToCamera = true;
-    
-    playerHPtext = game.add.text(280, 16, 'HP: ', '16px')
-    playerHPtext.fixedToCamera = true;
+	player.hpBox.fixedToCamera = true;
+	
+	playerHPtext = game.add.text(280, 16, 'HP: ', '16px')
+	playerHPtext.fixedToCamera = true;
 	
 	scoreText = game.add.text(16, 16, 'Score: 0', {fontSize: '32px'});
-    scoreText.fixedToCamera = true;
-    
-    bulletText = game.add.text(500, 16, 'Bullets: ', '16px');
-    bulletText.fixedToCamera = true;
-    
-    timeText = game.add.text(650, 16, 'Time: ', '16px');
-    timeText.fixedToCamera = true;
-    
-    //
+	scoreText.fixedToCamera = true;
+	
+	bulletText = game.add.text(500, 16, 'Bullets: ', '16px');
+	bulletText.fixedToCamera = true;
+	
+	timeText = game.add.text(650, 16, 'Time: ', '16px');
+	timeText.fixedToCamera = true;
+	
+	//
 	player.animations.add('left', [0, 1, 2, 3], 10, true);
 	player.animations.add('right', [5, 6, 7, 8], 10, true);
 	
-    weapon = game.add.sprite(player.x, player.y, 'weapon');
-    weapon.scale.setTo(.8);
-    weapon.anchor.setTo(player.x, player.y); // spawn weapon
-    weapon.reset(player.x,player.y);
-    
-    crosshair = game.add.sprite(400, 300, 'crosshair');
+	weapon = game.add.sprite(player.x, player.y, 'weapon');
+	weapon.scale.setTo(.8);
+	weapon.anchor.setTo(player.x, player.y); // spawn weapon
+	weapon.reset(player.x,player.y);
+	
+	crosshair = game.add.sprite(400, 300, 'crosshair');
 	
 	cursors = game.input.keyboard.createCursorKeys();
-    //wasd
-    leftKey = game.input.keyboard.addKey(Phaser.Keyboard.A);
-    rightKey = game.input.keyboard.addKey(Phaser.Keyboard.D);
-    upKey = game.input.keyboard.addKey(Phaser.Keyboard.W);
-    qKey = game.input.keyboard.addKey(Phaser.Keyboard.Q);
-    
+	//wasd
+	leftKey = game.input.keyboard.addKey(Phaser.Keyboard.A);
+	rightKey = game.input.keyboard.addKey(Phaser.Keyboard.D);
+	upKey = game.input.keyboard.addKey(Phaser.Keyboard.W);
+	qKey = game.input.keyboard.addKey(Phaser.Keyboard.Q);
+	
 	aliens = []
 	_aliens = game.add.group();
 	_aliens.enableBody = true;
 	
-    healthKits = game.add.group();
-    healthKits.enableBody = true;
-    
+	healthKits = game.add.group();
+	healthKits.enableBody = true;
+	
 	alienBullets = game.add.group();
 	alienBullets.enableBody = true;
-    alienBullets.physicsBodyType = Phaser.Physics.ARCADE;
-    alienBullets.setAll('checkWorldBounds', true);
-    alienBullets.setAll('outOfBoundsKill', true);
+	alienBullets.physicsBodyType = Phaser.Physics.ARCADE;
+	alienBullets.setAll('checkWorldBounds', true);
+	alienBullets.setAll('outOfBoundsKill', true);
 }
 
 function everyUpdate() {
@@ -329,13 +376,13 @@ function everyUpdate() {
 	game.physics.arcade.collide(healthKits, platforms);
 	game.physics.arcade.collide(_aliens, _aliens);
 	
-    game.physics.arcade.overlap(bullets, platforms, killBullet, null, this);
+	game.physics.arcade.overlap(bullets, platforms, killBullet, null, this);
 	game.physics.arcade.overlap(player, _aliens, collectAlien, null, this);
 	game.physics.arcade.overlap(player, alienBullets, killPlayer, null, this);
-    game.physics.arcade.overlap(player, healthKits, healthRestore, null, this);
+	game.physics.arcade.overlap(player, healthKits, healthRestore, null, this);
 	game.physics.arcade.overlap(bullets, _aliens, killaliens, null, this); // kill alien when hit by bullet
 	game.physics.arcade.overlap(alienBullets, platforms, killBullet, null, this);
-    
+	
 	player.body.velocity.x = 0;
 	handlePlayer();
 	crosshair.x = game.input.x - 2 + game.camera.x;
@@ -368,17 +415,17 @@ function everyUpdate() {
 		alien.handle();
 	});
 	
-    weapon.rotation = game.physics.arcade.angleToPointer(weapon);
-    if (game.input.activePointer.isDown) {
-        fire();
-    }
-    
-    handleAlienBullets(alienBullets);
-    
+	weapon.rotation = game.physics.arcade.angleToPointer(weapon);
+	if (game.input.activePointer.isDown) {
+		fire();
+	}
+	
+	handleAlienBullets(alienBullets);
+	
 	scoreText.text = 'Score: ' + score;
-    playerHPtext.text = 'HP: ' + player.hp;
-//    bulletText.text = 'Bullets: ' + bullets[1];
-//    timeText.text = 'Time: ' + ticks;
-    
+	playerHPtext.text = 'HP: ' + player.hp;
+//	bulletText.text = 'Bullets: ' + bullets[1];
+//	timeText.text = 'Time: ' + ticks;
+	
 }
 
