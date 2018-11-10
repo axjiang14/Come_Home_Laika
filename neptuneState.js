4d5330de17382d44c57b8aae6306bda023d99881
Yeoman2320 <43013937+Yeoman2320@users.noreply.github.com>
2018-11-09 22:47:00 -0600

    Platforms Worked....ALMOST PERFECT

--- neptuneState.js
+++ neptuneState.js
@@ -14,7 +14,7 @@ create: function() {
 	player = game.add.sprite(20, game.world.height - 70, 'laika');
 	
 	everyCreate();
-    player.hp = savedHP + 1; // starts with old HP +1 for the coloring
+    player.hp = savedHP; // starts with old HP +1 for the coloring
     onPlayerHit(); // to color
     
 	player.body.gravity.y = 1800;
@@ -22,17 +22,17 @@ create: function() {
 	stateLoad('states/neptune.json');
     
     
-    var timeLimit = game.time.events.add(Phaser.Timer.SECOND * 4, gameOver, this);
+//    var timeLimit = game.time.events.add(Phaser.Timer.SECOND * 4, gameOver, this);
 
 },
 
 update: function() {
 	everyUpdate();
 	game.physics.arcade.overlap(player, spaceship, this.spaceshipLeave, null, this);
-    if(ticks - startTicks > 100){
-    	console.log('ran out of time');
-    	game.state.start('gameOverState');
-    	}
+//    if(ticks - startTicks > 100){
+//    	console.log('ran out of time');
+//    	game.state.start('gameOverState');
+//    	}
     //else
     	//console.log(ticks, startTicks);
 },
