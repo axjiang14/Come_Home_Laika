ab12626f433a220f40101885704eef829684a330
Yeoman2320 <43013937+Yeoman2320@users.noreply.github.com>
2018-11-10 23:23:12 -0600

    working on venus..

--- venusState.js
+++ venusState.js
@@ -16,15 +16,18 @@ create: function() {
     onPlayerHit(); // to color
 	player.body.gravity.y = 1800; // 3 TILES JUMP 1800 = 4 TILES JUMP
     
-//    var tiles1 = platforms.create(384, 100, 'platform_tile');
-//    
-//	for(var i = 0; i < 21; ++i){
-//		var tile = platforms.create( 384, 24 * i , 'platform_tile'); // x = (nth tile+1) * 24
-//        
-//		//tile.scale.setTo(1, 0.25);
-//		tile.enableBody = true;
+    var tiles1 = platforms.create(384, 200, 'platform_tile');
+    var tiles1 = platforms.create(384, 220, 'platform_tile');
+    var tiles1 = platforms.create(384, 240, 'platform_tile');
+    var tiles1 = platforms.create(384, 260, 'platform_tile');
+
+	for(var i = 0; i < 21; ++i){
+		var tile = platforms.create( 384, 24 * i , 'platform_tile'); // x = (nth tile+1) * 24
+        
+		//tile.scale.setTo(1, 0.25);
+		tile.enableBody = true;
 //		tile.body.immovable = true;
-//    }
+    }
     
 	stateLoad('states/venus.json');
     
